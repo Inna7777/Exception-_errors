23522e9c55703265901648e854820b09067a6708
weld0 <peshkova-inna@mail.ru>
2023-08-07 00:45:27 +0300

    Add try catch to file WritingtoFile.

--- src/HW_third/WritingtoFile.java
+++ src/HW_third/WritingtoFile.java
@@ -6,21 +6,15 @@ import java.io.IOException;
 
 public class WritingtoFile {
     public static void writeToFile(Human human) throws IOException {
-<<<<<<< HEAD
         try {
             FileWriter writer = new FileWriter(new File(human.surname + ".txt"), true);
             writer.write(human.toString() + "\n");
             writer.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Произошла ошибка при записи в файл.");
         }
 
 
-=======
-        FileWriter writer = new FileWriter(new File(human.surname + ".txt"), true);
-        writer.write(human.toString() + "\n");
-        writer.close();
->>>>>>> origin/master
     }
 }
 
